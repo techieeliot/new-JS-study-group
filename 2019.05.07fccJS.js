3ad8ccc7d7f153b255d8605f64b3a83354bb62d6
Eliot Sanford <eliot587@gmail.com>
2019-05-07 06:32:48 -0500

    added manipulation of const array

--- 2019.05.07fccJS.js
+++ 2019.05.07fccJS.js
@@ -1,3 +1,17 @@
 // starting with ES6: Mutate an Array Declared with const
 
 
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+//s = [2, 5, 7];
+  // s = [2, 5, 7]; <- this is invalid
+s[2]=  7;
+s[0]= 2;
+s[1]= 5;
+console.log(s);
+
+ // change code above this line
+}
+editInPlace();
