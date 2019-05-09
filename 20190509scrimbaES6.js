64e3896418044f63d570614530dafa0f954cd808
Eliot Sanford <eliot587@gmail.com>
2019-05-09 10:21:58 -0500

    lastCommFirst added

--- 20190509scrimbaES6.js
+++ 20190509scrimbaES6.js
@@ -1,6 +1,8 @@
 const firstName = "Eliot";
 const lastName = "Sanford";
 
-let fullName = ${`firstName` `lastName`};
+let fullName = `${firstName} ${lastName}`;
+let lastCommaFirst = `${lastName} ${firstName}`;
 console.log(fullName);
 document.getElementById('insert-here').innerText = fullName;
+document.getElementsByClassName('insert-name').innerText = lastCommaFirst;
