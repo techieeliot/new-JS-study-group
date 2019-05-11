8a7240e4363789ac6d3c40b838a4f9c2158a5db1
Eliot Sanford <eliot587@gmail.com>
2019-05-11 07:56:25 -0500

    eliot & jemal's live share 5/11/2019

--- lengthForLoop.js
+++ lengthForLoop.js
@@ -1,1 +1,11 @@
-let 
+let countChar = str => {
+    let count = 0;
+    for(let i = 0, {length} = str; i < length; i++) {
+        count+=1;
+    }
+    return count;
+}  
+console.log(countChar("Hello woooorld!"));
+
+
+
